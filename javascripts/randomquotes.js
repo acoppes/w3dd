3b60d96bdedb06719cd75398a5ede9722480bc84
Ariel Coppes <ariel.coppes@gmail.com>
2016-02-21 14:45:00 -0300

    added quotes authors

--- javascripts/randomquotes.js
+++ javascripts/randomquotes.js
@@ -3,7 +3,8 @@
 // Free for all; but please leave in this header.
 // ==============================================
 
-var Quotation = new Array() // do not change this!
+var Quotation = new Array(); // do not change this!
+var Authors = new Array();
 
 // Set up the quotations to be shown, below.
 // To add more quotations, continue with the
@@ -19,9 +20,30 @@ Quotation[5] = "Being deeply loved by someone gives you strength, while loving s
 Quotation[6] = "We loved with a love that was more than love.";
 Quotation[7] = "When you realize you want to spend the rest of your life with somebody, you want the rest of your life to start as soon as possible.";
 Quotation[8] = "Love doesn’t just sit there, like a stone, it has to be made, like bread; remade all the time, made new.";
-
 Quotation[9] = "My love for you is a journey, starting at forever and ending at never."
 Quotation[10] = "Whatever our souls are made of, his and mine are the same."
+Quotation[11] = "With great power comes great responsibility."
+// Quotation[11] = ""
+
+Authors[0] = "Mahatma Gandhi";
+Authors[1] = "Dr. Seuss";
+Authors[2] = "Maya Angelou";
+Authors[3] = "Mignon McLaughlin";
+Authors[4] = "Alfred Tennyson";
+Authors[5] = "Lao Tzu";
+Authors[6] = "Edgar Allan Poe";
+Authors[7] = "When Harry Met Sally";
+Authors[8] = "Ursula K. Le Guin, The Lathe of Heaven";
+Authors[9] = "Anonymous";
+Authors[10] = "Emily Brontë";
+Authors[11] = "Uncle Ben";
+// Authors[11] = "";
+
+function getAuthor(index) {
+    if (index >= Authors.length)
+        return "Anonymous";
+    return Authors[index];
+}
 
 // ======================================
 // Do not change anything below this line
@@ -30,7 +52,7 @@ var Q = Quotation.length;
 var whichQuotation=Math.round(Math.random()*(Q-1));
 
 function showQuotation(){
-    document.write("\"" + Quotation[whichQuotation] + "\"");
+    document.write("\"" + Quotation[whichQuotation] + "\"<br/>-- " + getAuthor(whichQuotation));
 }
 
 // showQuotation();
